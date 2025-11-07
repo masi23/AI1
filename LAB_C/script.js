cf9e337308813dfc00be72746dd7dbbb1c9389b5
Zwierzchowski Mateusz <zm51642>
2025-11-07 11:39:00 +0100

    not finished

--- LAB_C/script.js
+++ LAB_C/script.js
@@ -1,18 +1,69 @@
-let latitude = 0;
-let longitude = 0;
+const defaultLocation = { latitude: 51.505, longitude: -0.09 };
+let selectedLocation = null;
 
-navigator.geolocation.getCurrentPosition((position) => {
-  latitude = position.coords.latitude;
-  longitude = position.coords.longitude;
+console.log(defaultLocation);
 
-  var map = L.map("map").setView([latitude, longitude], 15);
+let map = L.map("map").setView(
+  [defaultLocation.latitude, defaultLocation.longitude],
+  18
+);
+L.tileLayer.provider("Esri.WorldImagery").addTo(map);
+let marker = L.marker([
+  defaultLocation.latitude,
+  defaultLocation.longitude,
+]).addTo(map);
+marker.bindPopup("Twoja lokaliacja");
 
-  L.tileLayer(
-    "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+function getLocation() {
+  if (!navigator.geolocation) {
+    alert("Geolocation unavailable");
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      selectedLocation = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      };
+      map.setView([selectedLocation.latitude, selectedLocation.longitude]);
+    },
+    (positionError) => {
+      console.error(positionError);
+    },
     {
-      attribution:
-        "© Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
-      maxZoom: 18,
+      enableHighAccuracy: false,
     }
-  ).addTo(map);
+  );
+}
+
+document.getElementById("my-location").addEventListener("click", (event) => {
+  getLocation();
 });
+
+document.getElementById("take-photo").addEventListener("click", function () {
+  leafletImage(map, function (err, canvas) {
+    let rasterMap = document.getElementById("rasterMap");
+    let rasterContext = rasterMap.getContext("2d");
+
+    rasterContext.drawImage(canvas, 0, 0);
+  });
+});
+
+// let latitude = 0;
+// let longitude = 0;
+
+// navigator.geolocation.getCurrentPosition((position) => {
+//   latitude = position.coords.latitude;
+//   longitude = position.coords.longitude;
+
+//   var map = L.map("map").setView([latitude, longitude], 15);
+
+//   L.tileLayer(
+//     "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+//     {
+//       attribution:
+//         "© Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+//       maxZoom: 18,
+//     }
+//   ).addTo(map);
+// });
