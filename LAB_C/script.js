8e193d408c01f5c6750ac8b30e3fcebf15139e2e
masi23 <mateuszzwierzchowski19@gmail.com>
2025-11-09 17:49:36 +0100

    finished lab-c

--- LAB_C/script.js
+++ LAB_C/script.js
@@ -1,7 +1,11 @@
 const defaultLocation = { latitude: 51.505, longitude: -0.09 };
+let puzzleWidth = 0;
+let puzzleHeight = 0;
 let selectedLocation = null;
-
-console.log(defaultLocation);
+let properCount = 0;
+const puzzleSize = 16;
+const table = document.getElementById("puzzles-container");
+const board = document.getElementById("board");
 
 let map = L.map("map").setView(
   [defaultLocation.latitude, defaultLocation.longitude],
@@ -36,34 +40,162 @@ function getLocation() {
   );
 }
 
+let puzzlesArray = [];
+
 document.getElementById("my-location").addEventListener("click", (event) => {
   getLocation();
 });
 
 document.getElementById("take-photo").addEventListener("click", function () {
   leafletImage(map, function (err, canvas) {
-    let rasterMap = document.getElementById("rasterMap");
-    let rasterContext = rasterMap.getContext("2d");
-
-    rasterContext.drawImage(canvas, 0, 0);
+    // let rasterMap = document.getElementById("rasterMap");
+    // let rasterContext = rasterMap.getContext("2d");
+    // rasterMap.width = canvas.width;
+    // rasterMap.height = canvas.height;
+    prepareGame(canvas);
   });
 });
 
-// let latitude = 0;
-// let longitude = 0;
+function cutPuzzles(canvas) {
+  let puzzleImgs = [];
+  puzzleWidth = canvas.width / 4;
+  puzzleHeight = canvas.height / 4;
+  let index = 0;
+  for (let col = 0; col < 4; col++) {
+    for (let row = 0; row < 4; row++) {
+      let puzzle = document.createElement("canvas");
+      puzzle.width = puzzleWidth;
+      puzzle.height = puzzleHeight;
+      const puzzleContext = puzzle.getContext("2d");
+      puzzleContext.drawImage(
+        canvas,
+        row * puzzleWidth,
+        col * puzzleHeight,
+        puzzleWidth,
+        puzzleHeight,
+        0,
+        0,
+        puzzleWidth,
+        puzzleHeight
+      );
+      const img = new Image();
+      img.src = puzzle.toDataURL();
+      img.classList.add("puzzle-piece");
+      img.draggable = true;
+      img.row = row;
+      img.col = col;
+      // img.dataset.index = index;
+      img.index = index;
 
-// navigator.geolocation.getCurrentPosition((position) => {
-//   latitude = position.coords.latitude;
-//   longitude = position.coords.longitude;
+      img.addEventListener("dragstart", (e) => {
+        e.dataTransfer.setData("text/plain", e.target.index);
 
-//   var map = L.map("map").setView([latitude, longitude], 15);
+        const crt = document.createElement("canvas");
+        crt.width = 1;
+        crt.height = 1;
+        e.dataTransfer.setDragImage(crt, 0, 0);
+      });
 
-//   L.tileLayer(
-//     "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-//     {
-//       attribution:
-//         "© Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
-//       maxZoom: 18,
-//     }
-//   ).addTo(map);
-// });
+      puzzleImgs.push(img);
+      index++;
+    }
+  }
+  return puzzleImgs;
+}
+
+function shufflePuzzles(puzzles) {
+  const randomArray = [];
+  let i = 16;
+  while (puzzles.length) {
+    const randInt = Math.floor(Math.random() * i);
+    randomArray.push(...puzzles.splice(randInt, 1));
+    i--;
+  }
+
+  return randomArray;
+}
+
+function drawPuzzles() {
+  puzzlesArray.forEach((puzzle) => {
+    table.appendChild(puzzle);
+  });
+}
+
+function prepareBoard() {
+  for (let i = 0; i < 16; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.key = i;
+    cell.style.width = puzzleWidth + "px";
+    cell.style.height = puzzleHeight + "px";
+    cell.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+
+    cell.addEventListener("drop", (e) => {
+      const cell = e.currentTarget;
+      // console.log(cell.children.length);
+      if (cell.children.length) {
+        return;
+      }
+      const draggedId = e.dataTransfer.getData("text/plain");
+      const dragged = puzzlesArray.find(
+        (element) => element.index == draggedId
+      );
+      cell.appendChild(dragged);
+
+      if (draggedId == cell.key) {
+        properCount++;
+        dragged.draggable = false;
+      }
+      console.log(`cell-key: ${cell.key}, draggedId: ${draggedId}`);
+      console.log(
+        `czy cell.key == draggedId? ${cell.key == Number(draggedId)}`
+      );
+      console.log(`licznik poprawnych: ${properCount}`);
+      checkIfWin();
+    });
+
+    board.appendChild(cell);
+  }
+}
+
+function prepareGame(canvas) {
+  clearGame();
+  const puzzles = cutPuzzles(canvas);
+  puzzlesArray = shufflePuzzles(puzzles);
+  prepareBoard();
+  drawPuzzles();
+}
+
+function checkIfWin() {
+  if (properCount === puzzleSize) {
+    const notificationText = "Gratulacje! Wygrałeś 🎉";
+    if (!("Notification" in window)) {
+      alert(
+        "Wygrałeś ale ta przeglądarka nie wspiera powiadomień systemowych 😞"
+      );
+    } else if (Notification.permission === "granted") {
+      const notification = new Notification(notificationText);
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          const notification = new Notification(notificationText);
+        }
+      });
+    }
+  }
+}
+
+function clearGame() {
+  table.innerHTML = "";
+  board.innerHTML = "";
+  properCount = 0;
+  console.clear();
+}
+
+window.addEventListener("load", (e) => {
+  if (Notification.permission !== "granted") {
+    Notification.requestPermission();
+  }
+});
